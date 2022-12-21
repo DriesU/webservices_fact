f99c4284bc961a60cd1eeb2c06c0a0dfd16d1c43
dries uytterschaut <dries.uytterschaut@student.hogent.be>
2022-12-21 20:55:15 +0100

    tests for likedFact

---
+++ __tests__/rest/likedFact.spec.js
@@ -0,0 +1,75 @@
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+/* eslint-disable semi */
+/* eslint-disable comma-dangle */
+/* eslint-disable indent */
+const { withServer } = require("../helpers");
+const { tables } = require("../../src/data/index");
+
+
+describe('likedFact',  () => {
+    let request;
+    let knex;
+
+    withServer(({ knex: k, request: r }) => {
+        knex = k;
+        request = r;
+      });
+
+      const url = '/api/likedFacts';
+
+
+    //write a test function for findALLLikedFacts
+    describe('GET /api/likedFacts', () => {
+        it('should return 200 and all liked facts', async () => {
+            const response = await request.get(url);
+            expect(response.status).toBe(200);
+            expect(response.body.items.length).toBe(5);
+        })
+    })
+
+    //write a test function for findAllFromUserWithFact
+    describe('GET /api/likedFacts/:userId', () => {
+        it('should return 200 and all liked facts from a specific user', async () => {
+            const response = await request.get(`${url}/1`);
+            expect(response.status).toBe(200);
+            expect(response.body.items.length).toBe(2);
+        })
+    }
+    )
+
+    //write a test function for findAllFromUserWithFactAndCategory
+    describe('GET /api/likedFacts/:userId/:categoryId', () => {
+        it('should return 200 and all liked facts from a specific user and category', async () => {
+            const response = await request.get(`${url}/1/1`);
+            expect(response.status).toBe(200);
+            expect(response.body.items.length).toBe(2);
+        })
+    }
+    )
+    
+    //write a test function for deleteById (userId and factId)
+    describe('DELETE /api/likedFacts/:userId/:factId', () => {
+        it('should return 204 and delete a liked fact', async () => {
+            const response = await request.delete(`${url}/1/3`);
+            expect(response.status).toBe(204);
+        })
+    }
+    )
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
